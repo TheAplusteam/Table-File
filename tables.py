4cb3f0025943c2a7c6dd4b32e234eea37f8d799a
hshanno <hshanno@unomaha.edu>
2014-04-22 23:25:51 -0500

    Update tables.py

--- tables.py
+++ tables.py
@@ -1,6 +1,8 @@
 Table-File
 ==========
-Class Block_table(models.Model):
+from django.db import models
+
+class Block_table(models.Model):
   """Consists of user ID of blocked user and reason they have been
 	   blocked from borrowing."""
 	User_ID = models.CharField(max_length=25) #User ID of user who is blocked from borrowing
@@ -10,7 +12,7 @@ Class Block_table(models.Model):
 		return self.User_ID
 
 
-Class Parts_table(models.Model):
+class Parts_table(models.Model):
 	"""Consists of part name, compatibility of part, and count
 	   of each part in stock."""
 	PartName = models.CharField(max_length=70) #Name of the part
@@ -21,7 +23,7 @@ Class Parts_table(models.Model):
 		return self.PartName
 
 
-Class Software_table(models.Model):
+class Software_table(models.Model):
 	"""Consists of software name, compatibility of the software, ane
 	   count of each software in stock."""
 	SoftName = models.CharField(max_length=70) #Name of software installed on piece of equipment
@@ -32,7 +34,7 @@ Class Software_table(models.Model):
 		return self.SoftName
 
 
-Class BackupLogs_table(models.Model):
+class BackupLogs_table(models.Model):
 	"""Consists of a log of backups containing the date and description
 	   of each backup done on the system."""
 	Backup_Date = models.DateField(max_length=50) #Date and time of backup
